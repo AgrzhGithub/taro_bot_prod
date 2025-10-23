24c932bd365f24fb4e0e28fb7fdb29a9bf7d4b41
AgrzhGithub <AgrzhGithub@yandex.ru>
2025-10-24 00:35:47 +0300

    фикс начислений промокода

--- config.py
+++ config.py
@@ -9,10 +9,10 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./app.db")
 QWEN_API_KEY = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijk1YjBhOTg5LTNlMzItNDM2NC05YTZjLTk1ZWE1MTJiMzFiNCIsImxhc3RfcGFzc3dvcmRfY2hhbmdlIjoxNzU0NzcyMzAxLCJleHAiOjE3NTczNjQzMDR9.aB-ECrhNOgf6wKvYvENx31ynFLTyLvrjQSMIDb3mvAo'
 
 
-REFERRAL_BONUS_INVITED = int(os.getenv("REFERRAL_BONUS_INVITED", "10"))    # новичку по приглашению
-REFERRAL_BONUS_REFERRER = int(os.getenv("REFERRAL_BONUS_REFERRER", "10"))  # пригласившему
+REFERRAL_BONUS_INVITED = int(os.getenv("REFERRAL_BONUS_INVITED", "3"))    # новичку по приглашению
+REFERRAL_BONUS_REFERRER = int(os.getenv("REFERRAL_BONUS_REFERRER", "3"))  # пригласившему
 DEFAULT_FREE_CREDITS     = int(os.getenv("DEFAULT_FREE_CREDITS", "0"))     # на старт без промо/приглашения
-PROMO_DEFAULT_CREDITS    = int(os.getenv("PROMO_DEFAULT_CREDITS", "10"))   # если промокод маркетинговый без своих настроек
+PROMO_DEFAULT_CREDITS    = int(os.getenv("PROMO_DEFAULT_CREDITS", "3"))   # если промокод маркетинговый без своих настроек
 BOT_USERNAME             = os.getenv("BOT_USERNAME", "kartataro1_bot") 
 
 ADMIN_IDS = {int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x.strip().isdigit()}
